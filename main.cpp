5adf3337237ac7dc7d5431e8f42e229dc79aa32a
SelsabeelA <selsabeel.albaqir@gmail.com>
2022-04-18 12:09:02 +0200

    fix cmake

--- main.cpp
+++ main.cpp
@@ -1,25 +1,23 @@
-//
-// Created by Asus on 4/18/2022.
-//
 
 /***************************************************************
  Purpose: Invert and rotate (90,180,270) grayscale images.
  Purpose: Merge and darken/ligthen grayscale images.
+ Purpose: Flip and black/white grayscale images.
  Program load a gray image and store in another file
 
  Author:  Abdullah Mohammed Abdullah Farg
  ID: 20210541
 
- Author:  Adham Mohamed sayd Alam
+ Author:  Adham Mohamed sayed Allam
  ID: 20210053
 
  Author:  Selsabeel Asim Ali Elbagir
  ID: 20210714
 
 
- Section: S2
- Date:    30 March 2018
- Version: 1.0
+ Section: S1 & S2
+ Date:    17 April 2022
+ Version: 2.0
 ****************************************************************/
 #include <iostream>
 #include <fstream>
@@ -28,47 +26,71 @@
 #include "bmplib.cpp"
 
 using namespace std;
-unsigned char image[SIZE][SIZE];
+unsigned char image [SIZE][SIZE];
 unsigned char image2[SIZE][SIZE];
+unsigned char newimage[128][128];
 
 
 
-void loadImage();
-void saveImage();
+void loadImage ();
+void saveImage ();
 void Invert_Filter();
-void Rotate_Image();
-void merger();
-void darkLight();
-void shrinker();
+void Rotate_Image ();
+void merger ();
+void darkLight ();
+void Black_White_Image () ;
+void Flip ();
+void FlipHrizontal();
+void FlipVertical();
+void selection_menu();
+void shrink();
+void blur();
+//_________________________________________
 
 int main()
 {
+    selection_menu();
+}
+
+//_________________________________________
+
+void selection_menu()
+{
 
 
-    int filter = 1;
+    char filter = '1';
 
 
-    while (filter != 0)
+    while (filter != '0')
     {
         loadImage();
 
-        cout << "Please select a filter to apply or 0 to exit:" << endl;
-        cout << "1.Invert filter \n" << "2.Rotate image \n";
-        cout << "3.Merger image \n" << "4.Darken/Lighten image \n" << "0.exit \n";
-
+        cout <<"Please select a filter to apply or 0 to exit:"<<endl;
+        cout << "1.Invert filter \n"<<"2.Rotate image \n";
+        cout << "3.Merger image \n"<<"4.Darken/Lighten image \n";
+        cout << "5.Black_White_Image \n"<<"6.Flip_Image \n"<< "9.Shrink image\n" << "c.Blur image\n" << "0.exit \n";
         cin >> filter;
 
-        if (filter == 1)
+        if 		(filter == '1' )
             Invert_Filter();
-        else if (filter == 2)
-            Rotate_Image();
-        else if (filter == 3)
-            merger();
-        else if (filter == 4)
-            darkLight();
-        else if (filter == 0)
-            return 0;
+        else if (filter == '2' )
+            Rotate_Image ();
+        else if (filter == '3')
+            merger ();
+        else if (filter == '4' )
+            darkLight ();
+        else if (filter == '5' )
+            Black_White_Image () ;
+        else if (filter == '6' )
+            Flip () ;
+        else if (filter == '9')
+            shrink();
+        else if (filter == 'c')
+            blur();
+        else if (filter == '0')
+            cout << "M3 elsalamh ya user ya habibi \n"  ;
         saveImage();
+        break;
 
     }
 
@@ -76,7 +98,7 @@ int main()
 }
 
 //_________________________________________
-void loadImage() {
+void loadImage () {
     char imageFileName[100];
 
     // Get gray scale image file name
@@ -84,12 +106,12 @@ void loadImage() {
     cin >> imageFileName;
 
     // Add to it .bmp extension and load image
-    strcat(imageFileName, ".bmp");
+    strcat (imageFileName, ".bmp");
     readGSBMP(imageFileName, image);
 }
 
 //_________________________________________
-void saveImage() {
+void saveImage () {
     char imageFileName[100];
 
     // Get gray scale image target file name
@@ -97,7 +119,7 @@ void saveImage() {
     cin >> imageFileName;
 
     // Add to it .bmp extension and load image
-    strcat(imageFileName, ".bmp");
+    strcat (imageFileName, ".bmp");
     writeGSBMP(imageFileName, image);
 }
 
@@ -106,19 +128,19 @@ void Rotate_Image()
 
 
 {
-    char choose;
-    cout << "choose the degree of rotate please : " << endl;
-    cout << "a.90" << endl << "b.180" << endl << "c.270" << endl;
-    cin >> choose;
-    if (choose == 'a')//Rotate 90
+    char choose ;
+    cout <<  "choose the degree of rotate please : "<< endl;
+    cout << "a.90"<<endl<<"b.180"<<endl<<"c.270"<<endl;
+    cin >> choose ;
+    if (choose == 'a' )//Rotate 90
     {
         for (int i = 0; i < SIZE; i++)
         {
-            for (int j = 0; j < SIZE; j++)
+            for (int j = 0; j< SIZE; j++)
             {
 
-                image[i][j] = image[j - 256][i - 64];
-                image[i][j] = image[j - 256][i - 64];
+                image[i][j] =  image[j-256][i-64];
+                image[i][j] =  image[j-256][i-64];
 
             }
 
@@ -128,10 +150,10 @@ void Rotate_Image()
     {
         for (int i = 0; i < SIZE; i++)
         {
-            for (int j = 0; j < SIZE; j++)
+            for (int j = 0; j< SIZE; j++)
             {
 
-                image[i][j] = image[i - 256][-j - 64];
+                image[i][j] =  image[i-256][-j-64];
 
 
 
@@ -144,11 +166,11 @@ void Rotate_Image()
     {
         for (int i = 0; i < SIZE; i++)
         {
-            for (int j = 0; j < SIZE; j++)
+            for (int j = 0; j< SIZE; j++)
             {
 
 
-                image[i][j] = image[-j][-i - 64];
+                image[i][j] =  image[-j][-i-64];
             }
         }
 
@@ -161,7 +183,7 @@ void Invert_Filter()
 
     for (int i = 0; i < SIZE; i++)//Invert image
     {
-        for (int j = 0; j < SIZE; j++)
+        for (int j = 0; j< SIZE; j++)
         {
 
             image[i][j] = 256 - image[i][j];
@@ -178,13 +200,13 @@ void merger() {
     cin >> imageFileName2;
 
     // Add to it .bmp extension and load image
-    strcat(imageFileName2, ".bmp");
+    strcat (imageFileName2, ".bmp");
     readGSBMP(imageFileName2, image2);
 
 
     for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            image[i][j] = 0.5 * (image[i][j] + image2[i][j]);
+        for (int j = 0; j< SIZE; j++) {
+            image[i][j] = 0.5*( image[i][j] + image2[i][j] );
         }
     }
 
@@ -194,44 +216,111 @@ void darkLight() {
     char choice;
     cout << "Do you want to (d)arken or (l)ighten?";
     cin >> choice;
-    if (choice == 'd') {
+    if (choice=='d'){
         for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
+            for (int j = 0; j< SIZE; j++) {
                 image[i][j] *= 0.5;
             }
         }
     }
-    if (choice == 'l') {
+    if (choice=='l'){
         for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                image[i][j] = 0.5 * (image[i][j] + SIZE);
+            for (int j = 0; j< SIZE; j++) {
+                image[i][j] = 0.5*(image[i][j]+SIZE);
             }
 
         }
     }
-    else {
+    else{
         cout << "Invalid entry.";
     }
 
 }
 //_________________________________________
+void Black_White_Image ()
+{
+    long sum = 0;
+    double avg = 0;
+    //average of the pixels
+    for(int k = 0; k < SIZE; k++){
+        for(int l = 0; l < SIZE; l++){
+            sum += image[k][l];
+        }
+    }
+    avg = sum / (SIZE * SIZE);
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j< SIZE; j++) {
 
-void shrinker() {
+
+
+            if (image[i][j] > avg)
+                image[i][j] = 255;
+            else
+                image[i][j] = 0;
+
+        }
+    }
+}
+//_________________________________________
+void FlipHrizontal() {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j< (SIZE/2); j++) {
+            swap(image[i][j] , image[i][SIZE - j]);
+
+        }
+    }
+}
+//_________________________________________
+void FlipVertical(){
+    for (int i = 0; i < (SIZE/2); i++) {
+        for (int j = 0; j< SIZE; j++) {
+            swap(image[i][j] , image[SIZE - i][j]);
+
+        }
+    }
+}
+//_________________________________________
+void Flip (){
+    cout << "Flip (h)orizontally or (v)ertically ? " << endl;
+    char option;
+    cin >> option;
+    if(option == 'h'){
+        FlipHrizontal();
+    }
+    else if(option == 'v'){
+        FlipVertical();
+    }
+}
+//_________________________________________
+void shrink() {
     int choice;
     cout << "Would you like to shrink the image by a factor of 2, 3, or 4?\n";
     cout << "Please input your choice as 2 if you want the dimensions to be 1/2, \n";
     cout << "Please input your choice as 3 if you want the dimensions to be 1/3, \n";
     cout << "Please input your choice as 4 if you want the dimensions to be 1/4, \n";
     cin >> choice;
-    if (choice != 1) && (choice != 2) && (choice != 3) {
+    if ((choice != 1) && (choice != 2) && (choice != 3)) {
         cout << "Invalid entry.";
     }
     else {
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                image[i][j] = / choice;
+
+        int x = 0, y=0;
+        for (int i = 0; i < SIZE; i+=choice) {
+            for (int j = 0; j < SIZE; j+=choice) {
+                newimage[x][y] = image[i][j];
+                x++;
             }
+            y++; // error in this, I'll fix it later.
         }
     }
+    }
 
+void blur() {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            image[i][j] = (0.125)*( (image[i][j+1]) + (image[i][j-1]) + (image[i+1][j]) +(image[i+1][j+1]) + (image[i][j-1])
+                    + (image[i-1][j]) + (image[i-1][j+1]) + (image[i-1][j-1]) );
+
+        }
+    }
 }
